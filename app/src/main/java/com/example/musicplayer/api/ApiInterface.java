40a515a327f13653b224b9a25c3dc21adc3a5f42
hossein <kolali.hossein@yahoo.com>
2019-07-29 14:40:08 +0430

    edit url

--- app/src/main/java/com/example/musicplayer/api/ApiInterface.java
+++ app/src/main/java/com/example/musicplayer/api/ApiInterface.java
@@ -10,6 +10,6 @@ import retrofit2.http.GET;
 public interface ApiInterface {
      String BASE_URL = "https://api.whichapp.com/";
 
-    @GET("https://api.whichapp.com/v1/countries")
+    @GET("v1/countries")
     Call<List<ItemMusic>> getMusic();
 }
